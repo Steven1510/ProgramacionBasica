cd7dbf67a444e0cdf838e8de9dd9f4baa3c5857b
Steven1510 <jsromerot@udistrital.edu.co>
2025-09-05 12:59:23 -0500

    Add files via upload

---
+++ corte1/practica1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* 
+run this program using the console pauser or add your own getch, system("pause") or input loop 
+hola estoy aprendiendo programacion
+
+*/
+//quiero prender a programar en c
+int main(int argc, char *argv[]) {
+	
+	printf("hola mundo");
+	return 0;
+}
