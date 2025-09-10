46f663affa4c742b3f0b97d43df39712fd191add
Steven1510 <jsromerot@udistrital.edu.co>
2025-09-09 19:57:00 -0500

    Add files via upload

---
+++ corte1/main3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* print Fahrenheit-Celsius table
+for fahr = 0, 20, ..., 300; floating-point version */
+main()
+{
+float fahr, celsius;
+float lower, upper, step;
+lower = 0;    /* lower limit of temperatuire scale */
+upper = 3000;  /* upper limit */
+step = 20;    /* step size */
+fahr = lower;
+while (fahr <= upper) {
+celsius = 5 * (fahr-32.0) /9;
+printf("%3.0f %6.1f\n", fahr, celsius);
+fahr = fahr + step;
+}
+}
